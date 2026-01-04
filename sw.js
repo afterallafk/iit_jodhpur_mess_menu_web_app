2d8de113f43694d58d7304b63e33701b04744e21
afterallafk <shivamatiet2001@gmail.com>
2026-01-04 15:41:36 +0530

    Fix menu json's - Add the missing days and update menu properly
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v2.2";   // 🔼 change this every update
+const CACHE_VERSION = "v2.3";   // 🔼 change this every update
 const CACHE_NAME = `iitj-mess-menu-${CACHE_VERSION}`;
 
 const OFFLINE_URLS = [
