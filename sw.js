7030e4931e67862cf14590fbfea7b10f41c2b8fc
afterallafk <shivamatiet2001@gmail.com>
2026-05-08 17:26:30 +0530

    Update the Mess Menu to May 2026
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v2.6.1";   // 🔼 change this every update
+const CACHE_VERSION = "v2.6.2";   // 🔼 change this every update
 const CACHE_NAME = `iitj-mess-menu-${CACHE_VERSION}`;
 
 const OFFLINE_URLS = [
